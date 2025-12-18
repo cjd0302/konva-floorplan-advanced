e655dff1257147c237df6261b70828697b4f428b
Choi Jeong Dong <cjd0302@gmail.com>
2025-12-18 14:44:52 +0900

    fix: update

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from 'globals'
+import js from '@eslint/js'
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+
+        // ✅ 사내 WebView 전역
+        M: 'readonly',
+        MNative: 'readonly',
+        MCore: 'readonly'
+      }
+    }
+  }
+]
